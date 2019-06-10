10a66ee5efd83af9baed26994d214c9ef40dc7b8
Brett Johnson <br.onson@outlook.com>
2019-06-10 14:27:39 -0500

    Maps show up for all houses on employee index

---
+++ TrashCollector/house.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollector
+{
+    public class House
+    {
+        public double lat;
+        public double lng;
+    }
+}
